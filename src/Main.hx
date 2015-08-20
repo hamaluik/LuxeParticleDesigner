9f3270481326ff61cd7447e7648b92568ecbf7bf
Kenton Hamaluik <kenton@hamaluik>
2015-08-19 20:16:32 -0600

    Added size and colour controls

--- src/Main.hx
+++ src/Main.hx
@@ -23,8 +23,12 @@ class Main extends luxe.Game {
 	// particle options
 	var particles:ParticleSystem;
 	var emitter:ParticleEmitter;
-	var blend_src:Int;
-	var blend_dst:Int;
+	var blendSrc:Int;
+	var blendDst:Int;
+	var blendModes:Array<String> = ['zero', 'one', 'src_color', 'one_minus_src_color',
+									'src_alpha', 'one_minus_src_alpha', 'dst_alpha',
+									'one_minus_dst_alpha', 'dst_color', 'one_minus_dst_color',
+									'src_alpha_saturate'];
 	var startColour:ColorHSV = new ColorHSV(60, 1, 0.5, 1);
 	var endColour:ColorHSV = new ColorHSV(0, 1, 0.5, 0);
 	
@@ -95,7 +99,7 @@ class Main extends luxe.Game {
 	function initBatchers() {
 		Luxe.renderer.batcher.add_group(5,
 			function(b:phoenix.Batcher) {
-				Luxe.renderer.blend_mode(blend_src, blend_dst);
+				Luxe.renderer.blend_mode(blendSrc, blendDst);
 			},
 			function(b:phoenix.Batcher) {
 				Luxe.renderer.blend_mode();
@@ -117,8 +121,8 @@ class Main extends luxe.Game {
 
         // create some controls
         controls = new StringMap<Control>();
+
         
-        // emission
         controls.set('emissionwindow', new mint.Window({
         	parent: canvas,
         	name: 'emissionwindow',
@@ -131,60 +135,191 @@ class Main extends luxe.Game {
         	moveable: true
         }));
         makeSlider('emittime', 'Period',  controls.get('emissionwindow'),
-                   2, 26, 100, 396, 20,
+                   2, 26, 120, 396, 20,
                    emitter.emit_time, 0, 2, 0.01,
                    function(val:Float, _) {
                    		emitter.emit_time = val;
                    }
         );
         makeSlider('emitcount', 'Count',  controls.get('emissionwindow'),
-                   2, 48, 100, 396, 20,
+                   2, 48, 120, 396, 20,
                    emitter.emit_count, 0, 10, 1,
                    function(val:Float, _) {
                    		emitter.emit_count = Std.int(val);
                    }
         );
         makeSlider('emitdirection', 'Direction',  controls.get('emissionwindow'),
-                   2, 70, 100, 396, 20,
+                   2, 70, 120, 396, 20,
                    emitter.direction, 0, 360, 1,
                    function(val:Float, _) {
                    		emitter.direction = val;
                    }
         );
         makeSlider('emitdirection_random', 'Direction Random',  controls.get('emissionwindow'),
-                   2, 92, 100, 396, 20,
+                   2, 92, 120, 396, 20,
                    emitter.direction_random, 0, 360, 1,
                    function(val:Float, _) {
                    		emitter.direction_random = val;
                    }
         );
         makeSlider('emitspeed', 'Speed',  controls.get('emissionwindow'),
-                   2, 114, 100, 396, 20,
+                   2, 114, 120, 396, 20,
                    emitter.speed, 0, 10, 0.01,
                    function(val:Float, _) {
                    		emitter.speed = val;
                    }
         );
         makeSlider('emitspeed_random', 'Speed Random',  controls.get('emissionwindow'),
-                   2, 136, 100, 396, 20,
+                   2, 136, 120, 396, 20,
                    emitter.speed_random, -10, 10, 0.01,
                    function(val:Float, _) {
                    		emitter.speed_random = val;
                    }
         );
         
-        // 
-        controls.set('lifewindow', new mint.Window({
+
+        controls.set('sizewindow', new mint.Window({
         	parent: canvas,
-        	name: 'lifewindow',
-        	title: 'Life',
-        	x: 10, y: 178, w: 400, h: 158,
+        	name: 'sizewindow',
+        	title: 'Size',
+        	x: 10, y: 178, w: 400, h: 202,
         	collapsible: true,
         	resizable: true,
         	focusable: true,
         	closable: false,
         	moveable: true
         }));
+        makeSlider('startsizex', 'Start Size (x)',  controls.get('sizewindow'),
+                   2, 26, 120, 396, 20,
+                   emitter.start_size.x, 0, 128, 0.1,
+                   function(val:Float, _) {
+                   		emitter.start_size.x = val;
+                   }
+        );
+        makeSlider('startsizey', 'Start Size (y)',  controls.get('sizewindow'),
+                   2, 48, 120, 396, 20,
+                   emitter.start_size.y, 0, 128, 0.1,
+                   function(val:Float, _) {
+                   		emitter.start_size.y = Std.int(val);
+                   }
+        );
+        makeSlider('startsizex_random', 'Start Size (x) Random',  controls.get('sizewindow'),
+                   2, 70, 120, 396, 20,
+                   emitter.start_size_random.x, -128, 128, 0.1,
+                   function(val:Float, _) {
+                   		emitter.start_size_random.x = val;
+                   }
+        );
+        makeSlider('startsizey_random', 'Start Size (y) Random',  controls.get('sizewindow'),
+                   2, 92, 120, 396, 20,
+                   emitter.start_size_random.y, -128, 128, 0.1,
+                   function(val:Float, _) {
+                   		emitter.start_size_random.y = val;
+                   }
+        );
+        makeSlider('endsizex', 'End Size (x)',  controls.get('sizewindow'),
+                   2, 114, 120, 396, 20,
+                   emitter.end_size.x, 0, 128, 0.1,
+                   function(val:Float, _) {
+                   		emitter.end_size.x = val;
+                   }
+        );
+        makeSlider('endsizey', 'End Size (y)',  controls.get('sizewindow'),
+                   2, 136, 120, 396, 20,
+                   emitter.end_size.y, 0, 128, 0.1,
+                   function(val:Float, _) {
+                   		emitter.end_size.y = Std.int(val);
+                   }
+        );
+        makeSlider('endsizex_random', 'End Size (x) Random',  controls.get('sizewindow'),
+                   2, 158, 120, 396, 20,
+                   emitter.end_size_random.x, -128, 128, 0.1,
+                   function(val:Float, _) {
+                   		emitter.end_size_random.x = val;
+                   }
+        );
+        makeSlider('endsizey_random', 'End Size (y) Random',  controls.get('sizewindow'),
+                   2, 180, 120, 396, 20,
+                   emitter.end_size_random.y, -128, 128, 0.1,
+                   function(val:Float, _) {
+                   		emitter.end_size_random.y = val;
+                   }
+        );
+        
+        
+        controls.set('colourwindow', new mint.Window({
+        	parent: canvas,
+        	name: 'colourwindow',
+        	title: 'Colour',
+        	x: Luxe.screen.width - 410, y: 10, w: 400, h: 202,
+        	collapsible: true,
+        	resizable: true,
+        	focusable: true,
+        	closable: false,
+        	moveable: true
+        }));
+        makeSlider('starthue', 'Start Hue',  controls.get('colourwindow'),
+                   2, 26, 120, 396, 20,
+                   startColour.h, 0, 360, 1,
+                   function(val:Float, _) {
+                   		startColour.h = val;
+						emitter.start_color = startColour.toColor();
+                   }
+        );
+        makeSlider('startsaturation', 'Start Saturation',  controls.get('colourwindow'),
+                   2, 48, 120, 396, 20,
+                   startColour.s, 0, 1, 0.01,
+                   function(val:Float, _) {
+                   		startColour.s = val;
+						emitter.start_color = startColour.toColor();
+                   }
+        );
+        makeSlider('startalpha', 'Start Alpha',  controls.get('colourwindow'),
+                   2, 70, 120, 396, 20,
+                   startColour.a, 0, 1, 0.01,
+                   function(val:Float, _) {
+                   		startColour.a = val;
+						emitter.start_color = startColour.toColor();
+                   }
+        );
+        makeSlider('endhue', 'End Hue',  controls.get('colourwindow'),
+                   2, 92, 120, 396, 20,
+                   endColour.h, 0, 360, 1,
+                   function(val:Float, _) {
+                   		endColour.h = val;
+						emitter.end_color = endColour.toColor();
+                   }
+        );
+        makeSlider('endsaturation', 'End Saturation',  controls.get('colourwindow'),
+                   2, 114, 120, 396, 20,
+                   endColour.s, 0, 1, 0.01,
+                   function(val:Float, _) {
+                   		endColour.s = val;
+						emitter.end_color = endColour.toColor();
+                   }
+        );
+        makeSlider('endalpha', 'End Alpha',  controls.get('colourwindow'),
+                   2, 136, 120, 396, 20,
+                   endColour.a, 0, 1, 0.01,
+                   function(val:Float, _) {
+                   		endColour.a = val;
+						emitter.end_color = endColour.toColor();
+                   }
+        );
+        makeDropdown('blendsrc', 'SRC Blending',  controls.get('colourwindow'),
+                   2, 158, 396, 20,
+                   blendModes,
+                   function(idx:Int, c:Control, e:MouseEvent) {
+                   		 blendSrc = idx;
+                   }
+        );
+        makeDropdown('blenddst', 'DST Blending',  controls.get('colourwindow'),
+                   2, 180, 396, 20,
+                   blendModes,
+                   function(idx:Int, c:Control, e:MouseEvent) {
+                   		 blendDst = idx;
+                   }
+        );
 	}
 	
 	inline function makeSlider(name:String, label:String, parent:Control,
@@ -230,6 +365,35 @@ class Main extends luxe.Game {
 		
 		// add the custom onchange callback
 		slider.onchange.listen(onchange);
+	}
+	
+	inline function makeDropdown(name:String, label:String, parent:Control,
+	                             x:Float, y:Float, w:Float, h:Float,
+	                             items:Array<String>,
+	                             onchange:Int->Control->MouseEvent->Void) {
+		var dropdown:mint.Dropdown = new mint.Dropdown({
+			parent: parent,
+			name: name, text: label,
+			x: x, y: y, w: w, h: h
+		});
+		controls.set(name, dropdown);
+
+		var first:Bool = true;
+		for(item in items) {
+            dropdown.add_item(
+                new mint.Label({
+                    parent: dropdown, text: '$item', align:TextAlign.left,
+                    name: '$name-$item', w: w - 20, h: h, text_size: 14
+                }),
+                10, (first) ? 0 : 10
+            );
+            if(first) {
+            	first = false;
+            }
+		}
+
+		dropdown.onselect.listen(function(idx:Int,_,_) { dropdown.label.text = label + ": " + items[idx]; });
+		dropdown.onselect.listen(onchange);
 	}
 
     override function onmousemove(e) {
